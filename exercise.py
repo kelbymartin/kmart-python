11579c79479e762e6d5bfb65b80b977fbe77a321
kelbymartin <kelbymartin65@gmail.com>
2021-02-24 17:03:22 -0500

    Updated exercise.py

--- exercise.py
+++ exercise.py
@@ -14,14 +14,21 @@
 # print(x)
 
 #q3
-n = 22
-if n % 2 != 0:
-    print("Weird")
-elif n % 2 == 0 and 2 <= n <= 5:
-    print("Not Weird")
-elif n % 2 == 0 and 5 <= n <= 20:
-    print("Weird")
-elif n % 2 == 0 and n > 20:
-    print("Not Weird")
-else:
-    print("Somehow missed")
+def weird_conditions(n):
+    if n % 2 != 0:
+        print("Weird")
+    elif n % 2 == 0 and 2 <= n <= 5:
+        print("Not Weird")
+    elif n % 2 == 0 and 5 <= n <= 20:
+        print("Weird")
+    elif n % 2 == 0 and n > 20:
+        print("Not Weird")
+    else:
+        print("Somehow missed")
+
+#test cases: 1, 2, 3, 4, 6, 8, 11, 18, 20
+tests = [1, 2, 3, 4, 6, 8, 11, 18, 20]
+for i in tests:
+    weird_conditions(i)
+#results: Weird, Not Weird, Weird, Not Weird,
+# Weird, Weird, Weird, Weird, Weird

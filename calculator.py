7958459fc5d59da1d635d5ca20775e6a8a985b69
kelbymartin <kelbymartin65@gmail.com>
2021-02-24 13:11:26 -0500

    Added input and method calls

--- calculator.py
+++ calculator.py
@@ -24,8 +24,21 @@ def power(x, y):
     return output
 
 #User input
-
+ch = int(input("1-add 2-sub 3-mult 4-div 5-pow : "))
+x = int(input("Number: "))
+y = int(input("Number: "))
 
 #Calculator logic
-
 #Output
+if ch == 1:
+    print(add(x,y))
+elif ch == 2:
+    print(subtract(x,y))
+elif ch == 3:
+    print(multiply(x,y))
+elif ch == 4:
+    print(divide(x,y))
+elif ch == 5:
+    print(power(x,y))
+else:
+    print("You goofed")

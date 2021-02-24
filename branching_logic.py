4d5db0b387c159326010467aecd762f40275c4f2
kelbymartin <kelbymartin65@gmail.com>
2021-02-24 16:03:54 -0500

    Created branching_logic.py

---
+++ branching_logic.py
@@ -0,0 +1,12 @@
+
+like_python = True
+like_netflix = True
+
+if like_python:
+    print("Look on the bright side of life")
+elif like_netflix:
+    print("hmmm")
+else:
+    print("impressive")
+
+    

6812bc915bb16c636bb8a903df4293f54d908133
kelbymartin <kelbymartin65@gmail.com>
2021-02-24 11:01:44 -0500

    Added volume and area functions

--- function1.py
+++ function1.py
@@ -4,10 +4,27 @@ def weird_arithmetic(x, y, z):
     #print(output)
     return output
 
-result = weird_arithmetic(5, 6, 7)
-print("weird_arithmetic result:", result)
+#result = weird_arithmetic(5, 6, 7)
+#print("weird_arithmetic result:", result)
 
 def custom_greeting(greeting, name):
     print(greeting + " " + name)
 
 #custom_greeting("Hi", "Jeff")
+
+
+r = 4
+h = 10
+
+def area_circle(radius):
+    return (3.14 * radius**2)
+
+def vol_cylinder(area, height):
+    print(area * height)
+
+c_area = area_circle(r)
+vol_cylinder(c_area, h)
+#print("Radius:", r)
+#print("Height:", h)
+#print("Area:", c_area)
+#print("Volume of Cylinder:", result)

e0a45257a743ed19cf447a3ce908ed9e5a70fc9c
kelbymartin <kelbymartin65@gmail.com>
2021-02-25 17:12:22 -0500

    Added exception handling and Updated some messages

--- calculator_obj.py
+++ calculator_obj.py
@@ -60,54 +60,59 @@ if __name__ == "__main__":
     calc = Calculator()
     while True:
         #User input
-        ch = int(input("1-add 2-sub 3-mult 4-div 5-pow 6-root 7-viewlast 8-viewmem 9-clearmem 0-exit : "))
-        #Calculator logic
-        #Output
-        if ch == 1:
-            #User input
-            x = float(input("Number: "))
-            y = float(input("Number: "))
-            #call calculator
-            print(calc.add(x,y))
-        elif ch == 2:
-            #User input
-            x = float(input("Number: "))
-            y = float(input("Number: "))
-            #call calculator
-            print(calc.subtract(x,y))
-        elif ch == 3:
-            #User input
-            x = float(input("Number: "))
-            y = float(input("Number: "))
-            #call calculator
-            print(calc.multiply(x,y))
-        elif ch == 4:
-            #User input
-            x = float(input("Number: "))
-            y = float(input("Number: "))
-            #call calculator
-            print(calc.divide(x,y))
-        elif ch == 5:
-            #User input
-            x = float(input("Number: "))
-            y = float(input("Number: "))
-            #call calculator
-            print(calc.power(x,y))
-        elif ch == 6:
-            #User input
-            x = float(input("Number: "))
-            #call calculator
-            print(calc.root(x))
-        elif ch == 7:
-            #call calculator
-            print(calc.get_last())
-        elif ch == 8:
-            #call calculator
-            print(calc.get_mem())
-        elif ch == 9:
-            #call calculator
-            calc.clear_mem()
-        elif ch == 0:
-            break
-        else:
-            print("You goofed")
+        ch = 0
+        try:
+            ch = int(input("1-add 2-sub 3-mult 4-div 5-pow 6-root 7-viewlast 8-viewmem 9-clearmem 0-exit : "))
+            #Calculator logic
+            #Output
+            if ch == 1:
+                #User input
+                x = float(input("Number: "))
+                y = float(input("Number: "))
+                #call calculator
+                print(calc.add(x,y))
+            elif ch == 2:
+                #User input
+                x = float(input("Number: "))
+                y = float(input("Number: "))
+                #call calculator
+                print(calc.subtract(x,y))
+            elif ch == 3:
+                #User input
+                x = float(input("Number: "))
+                y = float(input("Number: "))
+                #call calculator
+                print(calc.multiply(x,y))
+            elif ch == 4:
+                #User input
+                x = float(input("Number: "))
+                y = float(input("Number: "))
+                #call calculator
+                print(calc.divide(x,y))
+            elif ch == 5:
+                #User input
+                x = float(input("Number: "))
+                y = float(input("Number: "))
+                #call calculator
+                print(calc.power(x,y))
+            elif ch == 6:
+                #User input
+                x = float(input("Number: "))
+                #call calculator
+                print(calc.root(x))
+            elif ch == 7:
+                #call calculator
+                print(calc.get_last())
+            elif ch == 8:
+                #call calculator
+                print(calc.get_mem())
+            elif ch == 9:
+                #call calculator
+                calc.clear_mem()
+            elif ch == 0:
+                break
+            else:
+                print("Use menu options")
+
+        except ValueError:
+            print("Not a number")

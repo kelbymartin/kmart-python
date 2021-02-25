e17fc54601cbed297e065e6c064db56f42a35f34
kelbymartin <kelbymartin65@gmail.com>
2021-02-25 17:49:46 -0500

    Explored other OS Module commands

--- osmod_intro.py
+++ osmod_intro.py
@@ -1,10 +1,17 @@
 import os
 
-print(os.getcwd())
+# print(os.getcwd())
 #os.chdir("..")
 #print(os.getcwd())
 #print(os.listdir(os.getcwd()))
 #os.mkdir("made_in_python")
 #os.rmdir("made_in_python")
+# print(os.stat("calculator.py"))
+#print(os.environ)
+#path_to_newtest = os.path.join(os.getcwd(), "newtest.py")
+#print(os.path.exists("dict.py"))
+#print(os.path.isfile("dict.py"))
+#print(os.path.isdir("dict.py"))
+#print(os.path.splitext(path_to_newtest))
 
 

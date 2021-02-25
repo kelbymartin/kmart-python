4b31fbfdcc50eb9e740f718f74b5b2d58d51bbe1
kelbymartin <kelbymartin65@gmail.com>
2021-02-25 15:55:10 -0500

    Updated file

--- classes_intro.py
+++ classes_intro.py
@@ -1,5 +1,8 @@
 class Car():
 
+    wheels = 4
+    doors = True
+
     def __init__(self, color, make, year, hpp):
         self.color = color
         self.make = make
@@ -9,6 +12,11 @@ class Car():
     def start_car(self):
         print("Vrooooom")
 
+    #Defines what print(car) will do
+    def __str__(self):
+        return self.make
+
+    #Defines how comparison will act
     def __gt__(self, other):
         return self.hpp > other.hpp
 

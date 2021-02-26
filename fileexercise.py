b123f94f0eddc48512dadbd4071411c477a48362
kelbymartin <kelbymartin65@gmail.com>
2021-02-26 11:17:08 -0500

    Simplified one line

--- fileexercise.py
+++ fileexercise.py
@@ -1,21 +1,6 @@
 #using variable
-with open('fileexercise.txt', 'r') as f:
-   orig = f.read().strip()
-
-with open('fileexercise.txt', 'w') as f:
-    f.write("write in:\n")
-    f.write("new line 1\n")
-    f.write("new line 2\n")
-
-with open('fileexercise.txt', 'a') as f:
-    f.write(orig)
-
-
-#using list
-# text = []
 # with open('fileexercise.txt', 'r') as f:
-#     for line in f:
-#         text.append(line)
+#    orig = f.read().strip()
 
 # with open('fileexercise.txt', 'w') as f:
 #     f.write("write in:\n")
@@ -23,5 +8,18 @@ with open('fileexercise.txt', 'a') as f:
 #     f.write("new line 2\n")
 
 # with open('fileexercise.txt', 'a') as f:
-#     for line in text:
-#         f.write(str(line))
+#     f.write(orig)
+
+
+#using list
+with open('fileexercise.txt', 'r') as f:
+    text = f.readlines()
+
+with open('fileexercise.txt', 'w') as f:
+    f.write("write in:\n")
+    f.write("new line 1\n")
+    f.write("new line 2\n")
+
+with open('fileexercise.txt', 'a') as f:
+    for line in text:
+        f.write(str(line))
